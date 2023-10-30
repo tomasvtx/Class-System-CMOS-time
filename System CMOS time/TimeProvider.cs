340dadc61fc2d2905dfc0d049741cb3f1df10611
Tomáš Vytiska <tomvytiska@gmail.com>
2023-10-30 21:34:50 +0100

    Přidejte soubory projektu.

---
+++ System CMOS time/TimeProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net.Sockets;
+
+namespace SystemCmosTime
+{
+    public sealed class TimeProvider
+    {
+        /// <summary>
+        /// Získá aktuální čas ze serveru time.nist.gov.
+        /// </summary>
+        /// <returns>DateTime s aktuálním časem, null pokud se nepodařilo získat čas.</returns>
+        public static DateTime? ZiskejDatumZeServeruNistGov()
+        {
+            try
+            {
+                using (TcpClient klient = new TcpClient("time.nist.gov", 13))
+                {
+                    using (StreamReader ctecka = new StreamReader(klient.GetStream()))
+                    {
+                        string odpoved = ctecka.ReadToEnd();
+                        string utcDateTimeString = odpoved.Substring(7, 17);
+
+                        DateTime lokalniCas = DateTime.ParseExact(utcDateTimeString, "yy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal).ToUniversalTime();
+
+                        return lokalniCas;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
