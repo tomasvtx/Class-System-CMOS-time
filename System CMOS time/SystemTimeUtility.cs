2753026c9f01170928068cfc80edb812a0a32089
Tomáš Vytiska <tomvytiska@gmail.com>
2023-11-02 17:28:04 +0100

    SOLID, DRY, CamelCase

---
+++ System CMOS time/SystemTimeUtility.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace SystemCmosTime
+{
+    public static class SystemTimeUtility
+    {
+        /// <summary>
+        /// Zapíše čas do systémového CMOS - vyžaduje oprávnění správce.
+        /// </summary>
+        /// <param name="localDateTime">Lokální DateTime, který bude zapsán do CMOS.</param>
+        /// <param name="error">Výstupní zpráva v případě chyby.</param>
+        /// <returns>True, pokud byl zápis úspěšný; jinak false.</returns>
+        public static bool WriteToCmos(this DateTime localDateTime, ref string error)
+        {
+            bool isElevated = CheckForAdministratorPrivileges();
+
+            if (!isElevated)
+            {
+                throw new Exception("Vyžadováno je oprávnění správce.");
+            }
+
+            SystemTime st = ConvertLocalDateTimeToSystemTime(localDateTime);
+
+            if (SetSystemTime(ref st))
+            {
+                error = "Systémový čas byl nastaven.";
+                return true;
+            }
+            else
+            {
+                error = "Nepodařilo se nastavit systémový čas. Chybový kód: " + Marshal.GetLastWin32Error();
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Zkontroluje, zda aktuální uživatel má oprávnění správce.
+        /// </summary>
+        /// <returns>
+        /// True, pokud aktuální uživatel má oprávnění správce; jinak false.
+        /// </returns>
+        private static bool CheckForAdministratorPrivileges()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                return new WindowsPrincipal(identity).IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        /// <summary>
+        /// Konvertuje místní DateTime na strukturu SystemTime, která se používá pro nastavení systémového času.
+        /// </summary>
+        /// <param name="localDateTime">Místní DateTime k konverzi.</param>
+        /// <returns>Struktura SystemTime reprezentující místní čas.</returns>
+        private static SystemTime ConvertLocalDateTimeToSystemTime(DateTime localDateTime)
+        {
+            SystemTime st = new SystemTime
+            {
+                Year = (short)localDateTime.Year,
+                Month = (short)localDateTime.Month,
+                Day = (short)localDateTime.Day,
+                Hour = (short)localDateTime.Hour,
+                Minute = (short)localDateTime.Minute,
+                Second = (short)localDateTime.Second
+            };
+
+            return st;
+        }
+
+        /// <summary>
+        /// Zapíše čas do systémového CMOS - vyžaduje oprávnění správce.
+        /// </summary>
+        /// <param name="st">Struktura SYSTEMTIME s časovými údaji.</param>
+        /// <returns>True, pokud byl zápis úspěšný; jinak false.</returns>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetSystemTime(ref SystemTime systemTime);
+
+        /// <summary>
+        /// Reprezentuje systémový čas v podobě struktury se sekvenčním uspořádáním položek.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SystemTime
+        {
+            /// <summary>
+            /// Obsahuje hodnotu pro rok.
+            /// </summary>
+            public short Year;
+
+            /// <summary>
+            /// Obsahuje hodnotu pro měsíc.
+            /// </summary>
+            public short Month;
+
+            /// <summary>
+            /// Obsahuje hodnotu pro den v týdnu.
+            /// </summary>
+            public short DayOfWeek;
+
+            /// <summary>
+            /// Obsahuje hodnotu pro den v měsíci.
+            /// </summary>
+            public short Day;
+
+            /// <summary>
+            /// Obsahuje hodnotu pro hodiny.
+            /// </summary>
+            public short Hour;
+
+            /// <summary>
+            /// Obsahuje hodnotu pro minuty.
+            /// </summary>
+            public short Minute;
+
+            /// <summary>
+            /// Obsahuje hodnotu pro sekundy.
+            /// </summary>
+            public short Second;
+
+            /// <summary>
+            /// Obsahuje hodnotu pro milisekundy.
+            /// </summary>
+            public short Milliseconds;
+        }
+
+    }
+}
--- System CMOS time/TimeProvider.cs
+++ System CMOS time/TimeProvider.cs
@@ -5,32 +5,77 @@ using System.Net.Sockets;
 
 namespace SystemCmosTime
 {
+    /// <summary>
+    /// Třída pro získávání aktuálního času ze serveru time.nist.gov pomocí poskytnutého rozhraní IDateTimeStringProvider.
+    /// </summary>
     public sealed class TimeProvider
     {
+        private readonly IDateTimeProvider _dateTimeStringProvider;
+
         /// <summary>
-        /// Získá aktuální čas ze serveru time.nist.gov.
+        /// Inicializuje novou instanci třídy TimeProvider s použitím zadaného poskytovatele textového datového řetězce.
         /// </summary>
-        /// <returns>DateTime s aktuálním časem, null pokud se nepodařilo získat čas.</returns>
-        public static DateTime? ZiskejDatumZeServeruNistGov()
+        /// <param name="dateTimeStringProvider">Rozhraní pro získávání textových datových řetězců, obsahujících UTC datum a čas.</param>
+        public TimeProvider(IDateTimeProvider dateTimeStringProvider)
+        {
+            _dateTimeStringProvider = dateTimeStringProvider;
+        }
+
+        /// <summary>
+        /// Získá aktuální UTC datum a čas ze zadaného datového zdroje a převede je na DateTime. 
+        /// Pokud dojde k chybě při získávání datového řetězce nebo při konverzi na DateTime, metoda vrátí null.
+        /// </summary>
+        /// <returns>Objekt DateTime reprezentující aktuální UTC datum a čas nebo null v případě chyby.</returns>
+        public DateTime? ZiskejDatumZeServeruNistGov()
         {
             try
             {
-                using (TcpClient klient = new TcpClient("time.nist.gov", 13))
+                string utcDateTimeString = _dateTimeStringProvider.ZiskejUtcDateTimeString();
+
+                if (utcDateTimeString != null)
                 {
-                    using (StreamReader ctecka = new StreamReader(klient.GetStream()))
-                    {
-                        string odpoved = ctecka.ReadToEnd();
-                        string utcDateTimeString = odpoved.Substring(7, 17);
-
-                        DateTime lokalniCas = DateTime.ParseExact(utcDateTimeString, "yy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal).ToUniversalTime();
-
-                        return lokalniCas;
-                    }
+                    return DateTime.ParseExact(utcDateTimeString, "yy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal).ToUniversalTime();
                 }
+
+                return null;
             }
             catch (Exception ex)
             {
                 throw ex;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Rozhraní pro získávání textových datových řetězců, které obsahují UTC datum a čas.
+    /// </summary>
+    public interface IDateTimeProvider
+    {
+        string ZiskejUtcDateTimeString();
+    }
+
+    /// <summary>
+    /// Implementace rozhraní IDateTimeStringProvider pro získávání UTC datum a čas ze serveru time.nist.gov.
+    /// </summary>
+    public class TcpDateTimeStringProvider : IDateTimeProvider
+    {
+        /// <summary>
+        /// Získá UTC datum a čas ve formě textového řetězce ze serveru time.nist.gov.
+        /// </summary>
+        /// <returns>Textový řetězec obsahující UTC datum a čas, nebo null v případě chyby.</returns>
+        public string ZiskejUtcDateTimeString()
+        {
+            try
+            {
+                using (TcpClient klient = new TcpClient("time.nist.gov", 13))
+                using (StreamReader ctecka = new StreamReader(klient.GetStream()))
+                {
+                    return ctecka.ReadToEnd().Substring(7, 17);
+                }
+            }
+            catch
+            {
+                return null;
             }
         }
     }
